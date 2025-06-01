a5616f3765cbf2b8fd24d870302318b2b277bf1a
Katy248 <petrovanton247@gmail.com>
2025-06-01 18:49:05 +0300

    Update login page

--- download-center/LoginPage.py
+++ download-center/LoginPage.py
@@ -10,27 +10,36 @@ class LoginPage(Adw.NavigationPage):
 
     login_button: Gtk.Button = Gtk.Template.Child()
     license_entry: Adw.EntryRow = Gtk.Template.Child()
-    persistence_check: Gtk.Switch = Gtk.Template.Child()
+    persistence_check: Adw.SwitchRow = Gtk.Template.Child()
     toast_overlay: Adw.ToastOverlay = Gtk.Template.Child()
 
     def __init__(self):
         super().__init__()
-        self.login_button.connect("clicked", self.on_login_button_click)
+        self.login_button.connect("activated", self.on_login_button_click)
         SETTINGS.bind(
             "stay-logged-in",
             self.persistence_check,
             "active",
             Gio.SettingsBindFlags.DEFAULT,
         )
+        self.license_entry.connect("changed", self.on_text_changed)
+
+    def on_text_changed(self, _: Adw.PasswordEntryRow):
+        self.license_entry.remove_css_class("error")
+        key = self.license_entry.get_text()
+        if len(key) <= 0:
+            self.license_entry.add_css_class("error")
 
     def on_login_button_click(self, btn: Gtk.Button):
         key = self.license_entry.get_text()
         if key is None or len(key) <= 0:
             self.add_toast("Invalid license key")
+            self.license_entry.add_css_class("error")
             return
 
         if not AUTH_STATE.authenticate(key):
-            self.add_toast("Invalid license key")
+            self.add_toast("Failed to authenticate")
+            self.license_entry.add_css_class("error")
 
     def add_toast(self, msg):
         toast = Adw.Toast.new(msg)
