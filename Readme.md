d563c2c19584916f9c71a8dab559e131b4806ee7
Katy248 <petrovanton247@gmail.com>
2025-06-01 19:12:59 +0300

    Added Readme

---
+++ Readme.md
@@ -0,0 +1,16 @@
+[![Please do not theme this app](https://stopthemingmy.app/badge.svg)](https://stopthemingmy.app)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
+# RED ADM Download center
+
+## Features
+
+- [X] Clean libadwaita look
+- [X] Downloading files
+- [X] Persistence authentication
+- [ ] Localization
+
+## Screenshots
+
+![Main page screenshot](other/screenshots/main-page.png)
+![Setting page](other/screenshots/settings-page.png)
--- download-center/__main__.py
+++ download-center/__main__.py
@@ -13,14 +13,18 @@ def on_activate(app: Adw.Application):
     window = MainWindow(app)
     window.present()
 
-    about_action = Gio.SimpleAction(name="win.about")
-    about_action.connect("activate", on_about_activated)
-    window.add_action(about_action)
-
-    app.set_accels_for_action("win.about", ["<Control>H"])
-
 
 if __name__ == "__main__":
     app = Adw.Application(application_id=APP_ID)
     app.connect("activate", on_activate)
+    about_action = Gio.SimpleAction.new("win.about", None)
+
+    about_action.connect(
+        "change-state", lambda action, value: print(action, value, "Changed state")
+    )
+    about_action.connect("activate", on_about_activated)
+    app.add_action(about_action)
+    app.set_accels_for_action("win.about", ["<Control>h"])
+    print(about_action.get_enabled())
+
     app.run([])
