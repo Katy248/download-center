13083da06be7ea5664233a18b41b704268e6bab7
Katy248 <petrovanton247@gmail.com>
2025-07-15 22:32:58 +0300

    Added handlers for buttons

--- download-center/DonationDialog.py
+++ download-center/DonationDialog.py
@@ -1,11 +1,13 @@
 from gi.repository import Gtk, Adw, Gio
 from .config import SETTINGS
+from gettext import gettext as _
 
 
 @Gtk.Template.from_resource("/ru/katy248/download-center/DonationDialog.ui")
 class DonationDialog(Adw.Dialog):
     __gtype_name__ = "DonationDialog"
     show_dialog_check_button: Gtk.CheckButton = Gtk.Template.Child()
+    donate_button: Gtk.Button = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -16,3 +18,19 @@ class DonationDialog(Adw.Dialog):
             "active",
             Gio.SettingsBindFlags.INVERT_BOOLEAN,
         )
+
+        self.donate_button.connect("clicked", self.on_donate_clicked)
+
+    def on_donate_clicked(self, button):
+        dialog = Adw.AlertDialog.new(
+            _("No money"),
+            _("Currently there is no way to donate((( But thank you for try!"),
+        )
+        dialog.add_response("ok", _("Ok"))
+        dialog.connect(
+            "response",
+            lambda dialog, response: SETTINGS.set_boolean(
+                "show-donation-dialog", False
+            ),
+        )
+        dialog.present(self)
